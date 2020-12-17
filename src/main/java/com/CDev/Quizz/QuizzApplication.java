6dcf9e799d480cc49ce7a379c3d3ef5f73c6a8a4
fabrice ferrere <fabrice.ferrere@ipilyon.net>
2020-12-17 10:06:04 +0100

    ajout des dépendances necessaires à la mise en production

--- src/main/java/com/CDev/Quizz/QuizzApplication.java
+++ src/main/java/com/CDev/Quizz/QuizzApplication.java
@@ -2,9 +2,10 @@ package com.CDev.Quizz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class QuizzApplication {
+public class QuizzApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(QuizzApplication.class, args);
