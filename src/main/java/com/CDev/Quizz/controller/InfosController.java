b31eab5a018f7ee2fe7a5c292b3dc9d5eb60605a
fabrice ferrere <fabrice.ferrere@ipilyon.net>
2021-01-12 02:12:31 +0100

    Fix probleme controller non trouve ( pas la bonne config sur le site de dev cote serveur

--- src/main/java/com/CDev/Quizz/controller/InfosController.java
+++ src/main/java/com/CDev/Quizz/controller/InfosController.java
@@ -1,36 +1,25 @@
 package com.CDev.Quizz.controller;
 
-import com.CDev.Quizz.entite.Questions;
-import com.CDev.Quizz.repository.QuestionsRepository;
 import com.CDev.Quizz.utils.Constante;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
-import org.springframework.data.domain.Sort;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.HttpServletRequest;
-import java.util.List;
 
 @RestController
 @RequestMapping(value = "infos")
 public class InfosController {
 
-
     @Autowired
-    QuestionsRepository questionsRepository;
-    @RequestMapping(method = RequestMethod.GET, value = "/type",produces = MediaType.APPLICATION_JSON_VALUE)
+    Environment environment;
 
-    public List<Questions> getAllQuestions(HttpServletRequest request){
-        // TODO implementer un système de vérification pour voir si
-        // le demandeur est bien un administrateur
-//        System.out.println(request.getRemoteUser());
-//        System.out.println(request.getRemoteAddr());
-//        System.out.println(request.getRemoteHost());
-//        System.out.println(request.getRemotePort());
-        return questionsRepository.findAll(Sort.by(Sort.Direction.DESC, "id"));
+    @GetMapping(value = "typeBuild",produces = MediaType.TEXT_PLAIN_VALUE)
+    public String getInfos(){
+
+        String infos = String.format("Vous êtes dans un environnement de type : %s\n" +
+                "L'adresse du site admin est : %s",environment.getProperty("type.environnement"),Constante.IpAdresseSiteAdmin);
+        return infos;
     }
 }
