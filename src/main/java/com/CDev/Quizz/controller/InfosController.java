0a3ebb5415211d729f75f5c80e69a6333493dba0
fabrice ferrere <fabrice.ferrere@ipilyon.net>
2021-01-12 02:04:15 +0100

    1.3.1

--- src/main/java/com/CDev/Quizz/controller/InfosController.java
+++ src/main/java/com/CDev/Quizz/controller/InfosController.java
@@ -1,42 +1,36 @@
 package com.CDev.Quizz.controller;
 
+import com.CDev.Quizz.entite.Questions;
+import com.CDev.Quizz.repository.QuestionsRepository;
 import com.CDev.Quizz.utils.Constante;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
 @RestController
 @RequestMapping(value = "infos")
 public class InfosController {
 
+
     @Autowired
-    Environment environment;
+    QuestionsRepository questionsRepository;
+    @RequestMapping(method = RequestMethod.GET, value = "/type",produces = MediaType.APPLICATION_JSON_VALUE)
 
-    @RequestMapping(method = RequestMethod.GET, value = "/typeBuild",produces = MediaType.APPLICATION_JSON_VALUE)
-    public Message getInfos(){
-
-//        String infos = String.format("Vous êtes dans un environnement de type : %s\n" +
-////                "L'adresse du site admin est : %s",environment.getProperty("type.environnement"),Constante.IpAdresseSiteAdmin);//
-        return new Message("Bienvenue");
-    }
-
-    public class Message{
-        private String texte;
-
-        public Message(String texte) {
-            this.texte = texte;
-        }
-
-        public String getTexte() {
-            return texte;
-        }
-
-        public void setTexte(String texte) {
-            this.texte = texte;
-        }
+    public List<Questions> getAllQuestions(HttpServletRequest request){
+        // TODO implementer un système de vérification pour voir si
+        // le demandeur est bien un administrateur
+//        System.out.println(request.getRemoteUser());
+//        System.out.println(request.getRemoteAddr());
+//        System.out.println(request.getRemoteHost());
+//        System.out.println(request.getRemotePort());
+        return questionsRepository.findAll(Sort.by(Sort.Direction.DESC, "id"));
     }
 }
