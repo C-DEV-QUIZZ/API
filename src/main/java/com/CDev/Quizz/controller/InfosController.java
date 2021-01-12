4d69ac388a1766a37329daee6a0afa12c9399d03
fabrice ferrere <fabrice.ferrere@ipilyon.net>
2021-01-12 01:44:08 +0100

    Update controller repond pas + web hook

--- src/main/java/com/CDev/Quizz/controller/InfosController.java
+++ src/main/java/com/CDev/Quizz/controller/InfosController.java
@@ -16,10 +16,10 @@ public class InfosController {
     Environment environment;
 
     @GetMapping(value = "typeBuild",produces = MediaType.TEXT_PLAIN_VALUE)
-    public void getInfos(){
+    public String getInfos(){
 
 //        String infos = String.format("Vous êtes dans un environnement de type : %s\n" +
 ////                "L'adresse du site admin est : %s",environment.getProperty("type.environnement"),Constante.IpAdresseSiteAdmin);//
-//        return infos;
+        return "recu";
     }
 }
