e4ee421a23c1914b4bf176a440562233da753f17
fabrice ferrere <fabrice.ferrere@ipilyon.net>
2021-03-09 16:05:34 +0100

    Suppression cors inutile

--- src/main/java/com/CDev/Quizz/controller/QuestionsController.java
+++ src/main/java/com/CDev/Quizz/controller/QuestionsController.java
@@ -18,7 +18,6 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping(value = "questions")
 public class QuestionsController {
 
